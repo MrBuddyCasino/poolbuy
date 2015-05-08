1404649078ee38f556f8e35d06a73171773f2c16
Michael Böckling <michael@boeckling.net>
2015-05-09 01:15:23 +0200

    implemented proper nested components & routing

--- poolbuy/resources/public/js/app.js
+++ poolbuy/resources/public/js/app.js
@@ -4,26 +4,24 @@ var $ = require('./lib/jquery/dist/jquery.js');
 //require('./lib/bootstrap/dist/js/bootstrap.js');
 var m = require('./lib/mithril/mithril.js');
 
-var poolbuy = {};
-
-// modules
-poolbuy.home = require('./m/home/component.js');
-poolbuy.nav = require('./m/nav/component.js');
+// componnents
+var home = require('./m/home/component.js');
+var nav = require('./m/nav/component.js');
 //poolbuy.login = require('./m/login/module.js');
 //poolbuy.account = require('./m/account/module.js');
 //poolbuy.signup = require('./m/signup/module.js');
 
-// modules
+// pages
 
-m.mount(document.getElementById('nav-container'), poolbuy.nav);
-m.mount(document.getElementById('main-container'), poolbuy.home);
+var index = require('./m/layout/component.js');
+index = m.component(index, {}, nav, home);
 
 
 // setup routes
-// m.route(document.body, "/", {
-//    "/": 		poolbuy.home
-//    "/login": 	poolbuy.login,
-//    "/account": poolbuy.account,
-//    "/signup": 	poolbuy.signup
-// });
+m.route(document.body, "/", {
+   "/":		index
+//   "/login": 	poolbuy.login,
+//   "/account": poolbuy.account,
+//   "/signup": 	poolbuy.signup
+});
 
